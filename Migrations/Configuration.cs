2f3f5efc4e4d272d78ed2d786bb86598173203c0
ianestraikh <y.estraikh@gmail.com>
2019-09-22 19:12:59 +1000

    seed identity tables

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,50 @@
+namespace RentAndCycleCodeFirst.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using RentAndCycleCodeFirst.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RentAndCycleCodeFirst.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            // https://stackoverflow.com/a/20521530/7587466
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "founder"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var passwordHasher = new PasswordHasher();
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+            };
+
+                manager.Create(user, "!Admin12345");
+                manager.AddToRole(user.Id, "Admin");
+            }
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
