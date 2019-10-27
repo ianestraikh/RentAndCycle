0148f1def6d658425a90a7fa02012ffaec1e5fca
ianestraikh <y.estraikh@gmail.com>
2019-10-28 02:22:37 +1100

    create db always

--- Models/BikeDbInitializer.cs
+++ Models/BikeDbInitializer.cs
@@ -8,7 +8,7 @@ using CsvHelper;
 
 namespace RentAndCycleCodeFirst.Models
 {
-    public class BikeDbInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BikeDbContext>
+    public class BikeDbInitializer : System.Data.Entity.DropCreateDatabaseAlways<BikeDbContext>
     {
         protected override void Seed(BikeDbContext context)
         {
